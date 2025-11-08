cf8da692dbbf7b0e0c63b158f926ab7e8929a36e
Honeytg <Honey.tong93@gmail.com>
2025-11-08 13:17:03 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # Script to update a Git repository, rebuild Docker containers, and copy theme files.
 
-# Configuration
-REPO_URL="https://github.com/Honeytg/Cloud-Application.git"
-DOCKER_SUBDIR="/home/ec2-user/wordpress-docker"
-CONTAINER_NAME="wordpress-app" # Used for docker cp and restart commands
-THEME_DIR="/var/www/html/wp-content/themes/customwp"
-
 echo "=========================================="
 echo " Starting Automated WordPress Deployment"
 echo "=========================================="
